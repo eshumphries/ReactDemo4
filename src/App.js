85da38313067bf28014cb8844c0e29c0ab8f525e
eshumphries <eshumphries@outlook.com>
2024-09-18 21:44:35 -0400

    Created a button function

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          This works
-        </p>
-        <button>This also works</button>
+        <div>
+          <h1>This works</h1>
+          <MyButton/>
+        </div>
       </header>
     </div>
   );
+}
+
+function MyButton() {
+  return (
+    <button>Click me</button>
+  )
 }
 
 export default App;
