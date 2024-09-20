3cc37baa819d911e4e3e7335ef5bdca52191c8e9
eshumphries <eshumphries@outlook.com>
2024-09-20 07:22:01 -0400

    Replaced MyButton with ButtonComponent to display message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ButtonComponent from './ButtonComponent';
 
 function App() {
   return (
@@ -7,37 +9,11 @@ function App() {
       <header className="App-header">
         <div>
           <h1>This works</h1>
-          {/* <Profile/> */}
-          <MyButton/>
+          <ButtonComponent/>
         </div>
       </header>
     </div>
   );
 }
-
-function MyButton() {
-  return (
-    <button>Does this work?</button>
-  )
-}
-
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://image.tmdb.org/t/p/original/qH15pDp99SXcMIuGFV7fU1x4Wqt.jpg',
-  imageSize: 90,
-};
-
-// export default function Profile() {
-//   return (
-//     <>
-//       <h1>{user.name}</h1>
-//       <img className="avatar" src={user.imageUrl} alt={'Photo of ' + user.name} style={{
-//         width: user.imageSize,
-//         height: user.imageSize
-//       }}
-//       />
-//     </>
-//   );
-// }
 
 export default App;
---
+++ src/ButtonComponent.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function ButtonComponent() {
+    const [message, setMessage] = useState('');
+
+    const handleClick = () => {
+        setMessage('It works');
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick}>Does this work?</button>
+            {message && <p>{message}</p>}
+        </div>
+    );
+}
