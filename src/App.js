c6efc96b1af3126b29827c732b49c160417aa409
eshumphries <eshumphries@outlook.com>
2024-09-20 06:50:00 -0400

    Removed picture and description

--- src/App.js
+++ src/App.js
@@ -27,15 +27,17 @@ const user = {
   imageSize: 90,
 };
 
-export default function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img className="avatar" src={user.imageUrl} alt={'Photo of ' + user.name} style={{
-        width: user.imageSize,
-        height: user.imageSize
-      }}
-      />
-    </>
-  );
-}
+// export default function Profile() {
+//   return (
+//     <>
+//       <h1>{user.name}</h1>
+//       <img className="avatar" src={user.imageUrl} alt={'Photo of ' + user.name} style={{
+//         width: user.imageSize,
+//         height: user.imageSize
+//       }}
+//       />
+//     </>
+//   );
+// }
+
+export default App;
